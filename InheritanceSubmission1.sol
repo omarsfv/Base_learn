7d855514c7a7aa77294e122f99a38ce1563831f2
omar_crypto <115907125+omarsfv@users.noreply.github.com>
2024-10-14 06:04:20 +0300

    Create InheritanceSubmission1.sol

---
+++ InheritanceSubmission1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract InheritanceSubmission {
+    address public salesPerson;
+    address public engineeringManager;
+
+    constructor(address _salesPerson, address _engineeringManager) {
+        salesPerson = _salesPerson;
+        engineeringManager = _engineeringManager;
+    }
+}
