5707495b69dad7246d72ace25148ce32cf7572f6
omar_crypto <115907125+omarsfv@users.noreply.github.com>
2024-10-16 00:34:42 +0300

    Update AddressBookFactory.sol

--- AddressBookFactory.sol
+++ AddressBookFactory.sol
@@ -6,8 +6,6 @@ import "./AddressBook.sol";
 
 // Contract for creating new instances of AddressBook
 contract AddressBookFactory {
-    // Define a private salt value for internal use
-    string private salt = "value";
 
     // Function to deploy a new instance of AddressBook
     function deploy() external returns (AddressBook) {
